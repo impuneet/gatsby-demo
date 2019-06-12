742e2200517d26344ec1bba99b6053369162033d
impuneet <iampuneet.in@gmail.com>
2019-06-12 18:33:35 +0530

    savin

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => (
       </div>
       .
     </h1>
-    <p>I’m a <strong>Full Stack developer</strong>, <strong>designer</strong>, and <strong>chef</strong> from India. I build open source projects and write the missing instruction manuals of the web. </p>
+    <p>I’m a <strong>Full Stack------------ developer</strong>, <strong>designer</strong>, and <strong>chef</strong> from India. I build open source projects and write the missing instruction manuals of the web. </p>
     <p>I created this site to document everything I learn, and share a bit of <a href="https://www.iampuneet.in">myself</a> with the world. This website has no bullshit, no ads, no sponsored posts, and no paywalls. If you enjoy my content, please consider <a href="https://ko-fi.com/iampuneet" target="_blank">supporting what I do.</a></p>
 
 
