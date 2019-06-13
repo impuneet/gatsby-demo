94c1d9ff9fa9078e711a1dc6bfb6480ce58ec7d9
impuneet <iampuneet.in@gmail.com>
2019-06-13 17:51:48 +0530

    savin

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,17 +59,17 @@ export default ({ data }) => {
       </p>
       <div  className={styles.container}>
         <div className={styles.card}>
-            <h1>Kontify.me</h1>
-            <p>Don't miss our launch, get notified when we're ready!</p>
-            <form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className={styles.validate}>
-              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your email" />
-              <div  aria-hidden="true"><input type="text" name="" tabindex="-1" value="">
-              </div>
-              <div class={styles.clear}></div>
-              <button name="subscribe" id="mc-embedded-subscribe" className={styles.button} type="submit">Subscribe me</button>
-            </form>
-          </div>
-</div>
+          <h1>Kontify.me</h1>
+          <p>Don't miss our launch, get notified when we're ready!</p>
+          <form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className={styles.validate}>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your email" />
+            <div  aria-hidden="true"><input type="text" name="" tabindex="-1" value="" />
+            </div>
+            <div class={styles.clear}></div>
+            <button name="subscribe" id="mc-embedded-subscribe" className={styles.button} type="submit">Subscribe me</button>
+          </form>
+        </div>
+      </div>
       <p>
         If you want to keep up with a less polished version of me, I'm pretty
       active on Twitter at{' '}
