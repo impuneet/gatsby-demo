2040d8dc864d527ea5ff6cf02d5728723c755843
impuneet <iampuneet.in@gmail.com>
2019-07-12 15:11:53 +0530

    'solu'

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -21,8 +21,7 @@ export default ({ data }) => {
         If you liked this post, get updates about new posts by signing up to my
         infrequent newsletter.
       </p>
-      {/* style="border:1px solid #ccc;padding:3px;text-align:center;" */}
-      <form  style={{border: '1px solid #ccc',padding :  '3px', textAlign:'center'}}
+      <form
         action="https://tinyletter.com/iampuneet" method="post"
         target="popupwindow"
         onsubmit="window.open('https://tinyletter.com/iampuneet', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"><p><label for="tlemail">Enter your email address</label></p><p><input type="text" style="width:140px" name="email" id="tlemail" /></p><input type="hidden" value="1" name="embed" /><input type="submit" value="Subscribe" /><p><a href="https://tinyletter.com" target="_blank">powered by TinyLetter</a></p></form>
